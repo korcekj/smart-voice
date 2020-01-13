9732790a5bdd3bef515b7b36fe23b78d54d83d28
Jan Korcek <korcekdevelopment@yahoo.com>
2020-01-13 19:39:49 +0100

    Local devices update.

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
+
+app.get('/api/local-devices', (req, res) => {
+  const devices = os.networkInterfaces();
+  if (!devices) res.status(500).send({ error: 'No devices' });
+  res.status(200).send(devices);
+});
+
 if (process.env.NODE_ENV === 'production') {
   app.use(compression());
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
@@ -28,14 +38,4 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ', port);
-});
-
-app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-});
-
-app.get('/api/local-devices', (req, res) => {
-  const devices = os.networkInterfaces();
-  if (!devices) res.status(500).send({ error: 'No devices' });
-  res.status(200).send(devices);
 });
